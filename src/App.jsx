9da3af0e85e3deece53f21f1cfb6cd352e3031ca
Durgesh Kumar <amansri2023@gmail.com>
2024-08-17 03:57:04 +0530

    resolving problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Navbar from "./components/Navbar"
 import { Home } from "./components/Home"
-import { SocialLink } from "./components/SocialLink"
 import { About } from "./components/About"
 import { Portfolio } from "./components/Portfolio"
 import { Experience } from "./components/Experience"
 import { Contact } from "./components/Contact"
 import { SocialCard } from "./components/SocialCard"
-import { Input } from "./components/Input"
 
 function App() {
 
@@ -19,8 +17,6 @@ function App() {
         <Portfolio />
         <Experience />
         <Contact />
-
-        {/* <SocialLink /> */}
         <SocialCard />
 
       </div>
